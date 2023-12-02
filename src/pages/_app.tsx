30ad47cf0d0df1b57addd77cb4410b23bb156ba6
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2023-12-01 23:15:50 -0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-        <title>Hpsk's website</title>
+        <title>Hpsk&apos;s website</title>
     </Head>
     <Component {...pageProps} />
   </>
